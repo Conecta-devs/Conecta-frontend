ad032a0602272dce232e9b4e1f0f53e23aa76a6a
MatheusPalexandre <matheus.pa2@puccampinas.edu.br>
2026-08-20 20:28:20 -0300

    Adiciona aplicação Conecta

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import router from './router'
+import './tailwind.css'
+import './style.scss'
+import App from './App.vue'
+
+createApp(App)
+	.component('font-awesome-icon', FontAwesomeIcon)
+	.use(router)
+	.mount('#app')
